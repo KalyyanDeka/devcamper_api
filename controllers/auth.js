1d949645a6c4ffa50c648354869174a34c5570d2
kalyyan <Kalyyan.2612@gmail.com>
2020-08-12 08:26:03 +0530

    logout clear token

--- controllers/auth.js
+++ controllers/auth.js
@@ -61,8 +61,23 @@ exports.login = asyncHandler(async (req, res, next) => {
   res.status(200).json({ success: true, token });
 });
 
+// @desc       Log user out / clear cookie
+// @routes     GET /api/v1/auth/me
+// @access     Private
+exports.logout = asyncHandler(async (req, res, next) => {
+  res.cookie('token', 'none', {
+    expires: new Date(Date.now() + 10 * 1000),
+    httpOnly: true,
+  });
+
+  res.status(200).json({
+    success: true,
+    data: {},
+  });
+});
+
 // @desc       Get current logged in user
-// @routes     POST /api/v1/auth/me
+// @routes     GET /api/v1/auth/me
 // @access     Private
 exports.getMe = asyncHandler(async (req, res, next) => {
   const user = await User.findById(req.user.id);
